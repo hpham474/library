af2a091f9e5a998741d3fb62c0952944c52ac519
hpham474 <hpham474@gmail.com>
2024-06-07 04:58:14 -0500

    convert library into a class

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+// const myLibrary = [];
 
 // function Book (title, author, pages, read) {
 //     this.title = title;
@@ -10,148 +10,345 @@ const myLibrary = [];
 //     this.read = !this.read;
 // }
 
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-}
+// function addBookToLibrary(book) {
+//     myLibrary.push(book);
+// }
 
-function createBookCard(book) {
-    let bookCard = document.createElement("div");
-    bookCard.className="book";
+// function createBookCard(book) {
+//     let bookCard = document.createElement("div");
+//     bookCard.className="book";
 
-    let title = document.createElement("p");
-    let bookTitle = document.createElement("p");
-    let author = document.createElement("p");
-    let bookAuthor = document.createElement("p");
-    let pages = document.createElement("p");
-    let bookPages = document.createElement("p");
+//     let title = document.createElement("p");
+//     let bookTitle = document.createElement("p");
+//     let author = document.createElement("p");
+//     let bookAuthor = document.createElement("p");
+//     let pages = document.createElement("p");
+//     let bookPages = document.createElement("p");
 
-    title.textContent = "Title:";
-    bookTitle.textContent = book.title;
-    author.textContent = "Author:";
-    bookAuthor.textContent = book.author;
-    pages.textContent = "Pages:";
-    bookPages.textContent = book.pages;
-    bookRead = book.read;
+//     title.textContent = "Title:";
+//     bookTitle.textContent = book.title;
+//     author.textContent = "Author:";
+//     bookAuthor.textContent = book.author;
+//     pages.textContent = "Pages:";
+//     bookPages.textContent = book.pages;
+//     bookRead = book.read;
 
-    let empty = document.createElement("div");
+//     let empty = document.createElement("div");
 
-    let container = document.createElement("div");
-    container.className="button-container";
+//     let container = document.createElement("div");
+//     container.className="button-container";
 
-    let remove = document.createElement("button");
-    remove.className="remove-button";
-    remove.textContent = "Remove";
-    let status = document.createElement("button");
-    status.className="read-status-button";
+//     let remove = document.createElement("button");
+//     remove.className="remove-button";
+//     remove.textContent = "Remove";
+//     let status = document.createElement("button");
+//     status.className="read-status-button";
 
-    if(bookRead) {
-        bookCard.className += " read";
-        status.className += " readButton";
-        status.textContent = "Unread";
-    } else {
-        bookCard.className += " unread";
-        status.className += " unreadButton";
-        status.textContent = "Read";
-    }
+//     if(bookRead) {
+//         bookCard.className += " read";
+//         status.className += " readButton";
+//         status.textContent = "Unread";
+//     } else {
+//         bookCard.className += " unread";
+//         status.className += " unreadButton";
+//         status.textContent = "Read";
+//     }
 
-    remove.addEventListener("click", (event) => {
-        let index = findIndex(event);
+//     remove.addEventListener("click", (event) => {
+//         let index = findIndex(event);
 
-        myLibrary.splice(index, 1);
+//         myLibrary.splice(index, 1);
         
-        event.
-            target.
-            parentNode.
-            parentNode.
-            remove();
-    });
+//         event.
+//             target.
+//             parentNode.
+//             parentNode.
+//             remove();
+//     });
 
-    status.addEventListener("click", (event) => {
-        let index = findIndex(event);
+//     status.addEventListener("click", (event) => {
+//         let index = findIndex(event);
 
-        myLibrary[index].changeReadStatus();
+//         myLibrary[index].changeReadStatus();
 
-        if(myLibrary[index].read) {
-            event.
-                target.
-                parentNode.
-                parentNode.
-                className = "book read";
-            event.
-                target.
-                className = "read-status-button readButton"
-            event.
-                target.
-                textContent = "Unread"
-        } else {
-            event.
-                target.
-                parentNode.
-                parentNode.
-                className = "book unread";
-            event.
-                target.
-                className = "read-status-button readButton"
-            event.
-                target.
-                textContent = "Read"
-        }
-    });
+//         if(myLibrary[index].read) {
+//             event.
+//                 target.
+//                 parentNode.
+//                 parentNode.
+//                 className = "book read";
+//             event.
+//                 target.
+//                 className = "read-status-button readButton"
+//             event.
+//                 target.
+//                 textContent = "Unread"
+//         } else {
+//             event.
+//                 target.
+//                 parentNode.
+//                 parentNode.
+//                 className = "book unread";
+//             event.
+//                 target.
+//                 className = "read-status-button readButton"
+//             event.
+//                 target.
+//                 textContent = "Read"
+//         }
+//     });
 
-    container.append(remove);
-    container.append(status);
+//     container.append(remove);
+//     container.append(status);
 
-    bookCard.style.display = "grid";
-    bookCard.style.gridTemplate = "1fr 1fr 1fr 1fr / 4rem 1fr";
+//     bookCard.style.display = "grid";
+//     bookCard.style.gridTemplate = "1fr 1fr 1fr 1fr / 4rem 1fr";
 
-    bookCard.appendChild(title);
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(author);
-    bookCard.appendChild(bookAuthor);
-    bookCard.appendChild(pages);
-    bookCard.appendChild(bookPages);
-    bookCard.appendChild(empty);
-    bookCard.appendChild(container);
+//     bookCard.appendChild(title);
+//     bookCard.appendChild(bookTitle);
+//     bookCard.appendChild(author);
+//     bookCard.appendChild(bookAuthor);
+//     bookCard.appendChild(pages);
+//     bookCard.appendChild(bookPages);
+//     bookCard.appendChild(empty);
+//     bookCard.appendChild(container);
 
-    return bookCard;
-}
+//     return bookCard;
+// }
 
-function displayLibrary() {
-    const library = document.querySelector(".library");
-    for (let i = 0; i < myLibrary.length; i++) {
-        let book = createBookCard(myLibrary[i]);
+// function displayLibrary() {
+//     const library = document.querySelector(".library");
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         let book = createBookCard(myLibrary[i]);
 
-        library.append(book);
+//         library.append(book);
+//     }
+// }
+
+// function findIndex(event) {
+//     let titleToChange = 
+//             event.
+//             target.
+//             parentNode.
+//             parentNode.
+//             querySelector("p:nth-child(2)").
+//             textContent;
+
+//     let authorToChange = 
+//         event.
+//         target.
+//         parentNode.
+//         parentNode.
+//         querySelector("p:nth-child(4)").
+//         textContent;
+
+//     let index = -1;
+//     for (let i = 0; i < myLibrary.length; i++) {
+//         if(myLibrary[i].title === titleToChange 
+//             && myLibrary[i].author === authorToChange ) {
+//             index = i;
+//         }
+//     }
+
+//     return index;
+// }
+
+class Book {
+    title;
+    author;
+    pages;
+    read;
+
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+
+    changeReadStatus () {
+        this.read = !this.read;
+    }
+
+    get title() {
+        return this.title;
+    }
+
+    get author() {
+        return this.author;
+    }
+
+    get pages() {
+        return this.pages;
+    }
+
+    get read() {
+        return this.read;
     }
 }
 
-function findIndex(event) {
-    let titleToChange = 
+class Library {
+    myLibrary = [];
+
+    constructor () {
+        this.myLibrary = [];
+    }
+
+    get size () {
+        return this.myLibrary.length;
+    }
+
+    getBook (index) {
+        return this.myLibrary[index];
+    }
+
+
+    addBookToLibrary(book) {
+        this.myLibrary.push(book);
+    }
+
+    displayLibrary() {
+        const library = document.querySelector(".library");
+        for (let i = 0; i < this.myLibrary.length; i++) {
+            let book = this.createBookCard(this.myLibrary[i]);
+    
+            library.append(book);
+        }
+    }
+
+    findIndex(event) {
+        let titleToChange = 
+                event.
+                target.
+                parentNode.
+                parentNode.
+                querySelector("p:nth-child(2)").
+                textContent;
+    
+        let authorToChange = 
             event.
             target.
             parentNode.
             parentNode.
-            querySelector("p:nth-child(2)").
+            querySelector("p:nth-child(4)").
             textContent;
-
-    let authorToChange = 
-        event.
-        target.
-        parentNode.
-        parentNode.
-        querySelector("p:nth-child(4)").
-        textContent;
-
-    let index = -1;
-    for (let i = 0; i < myLibrary.length; i++) {
-        if(myLibrary[i].title === titleToChange 
-            && myLibrary[i].author === authorToChange ) {
-            index = i;
+    
+        let index = -1;
+        for (let i = 0; i < this.myLibrary.length; i++) {
+            if(this.myLibrary[i].title === titleToChange 
+                && this.myLibrary[i].author === authorToChange ) {
+                index = i;
+            }
         }
+    
+        return index;
     }
 
-    return index;
+    createBookCard(book) {
+        let bookCard = document.createElement("div");
+        bookCard.className="book";
+    
+        let title = document.createElement("p");
+        let bookTitle = document.createElement("p");
+        let author = document.createElement("p");
+        let bookAuthor = document.createElement("p");
+        let pages = document.createElement("p");
+        let bookPages = document.createElement("p");
+    
+        title.textContent = "Title:";
+        bookTitle.textContent = book.title;
+        author.textContent = "Author:";
+        bookAuthor.textContent = book.author;
+        pages.textContent = "Pages:";
+        bookPages.textContent = book.pages;
+        let bookRead = book.read;
+    
+        let empty = document.createElement("div");
+    
+        let container = document.createElement("div");
+        container.className="button-container";
+    
+        let remove = document.createElement("button");
+        remove.className="remove-button";
+        remove.textContent = "Remove";
+        let status = document.createElement("button");
+        status.className="read-status-button";
+    
+        if(bookRead) {
+            bookCard.className += " read";
+            status.className += " readButton";
+            status.textContent = "Unread";
+        } else {
+            bookCard.className += " unread";
+            status.className += " unreadButton";
+            status.textContent = "Read";
+        }
+    
+        remove.addEventListener("click", (event) => {
+            let index = this.findIndex(event);
+    
+            this.myLibrary.splice(index, 1);
+            
+            event.
+                target.
+                parentNode.
+                parentNode.
+                remove();
+        });
+    
+        status.addEventListener("click", (event) => {
+            let index = this.findIndex(event);
+    
+            this.myLibrary[index].changeReadStatus();
+    
+            if(this.myLibrary[index].read) {
+                event.
+                    target.
+                    parentNode.
+                    parentNode.
+                    className = "book read";
+                event.
+                    target.
+                    className = "read-status-button readButton"
+                event.
+                    target.
+                    textContent = "Unread"
+            } else {
+                event.
+                    target.
+                    parentNode.
+                    parentNode.
+                    className = "book unread";
+                event.
+                    target.
+                    className = "read-status-button readButton"
+                event.
+                    target.
+                    textContent = "Read"
+            }
+        });
+    
+        container.append(remove);
+        container.append(status);
+    
+        bookCard.style.display = "grid";
+        bookCard.style.gridTemplate = "1fr 1fr 1fr 1fr / 4rem 1fr";
+    
+        bookCard.appendChild(title);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(author);
+        bookCard.appendChild(bookAuthor);
+        bookCard.appendChild(pages);
+        bookCard.appendChild(bookPages);
+        bookCard.appendChild(empty);
+        bookCard.appendChild(container);
+    
+        return bookCard;
+    }
+    
 }
+
+let myLibrary = new Library();
 
 const dialog = document.querySelector(".book-form");
 const addButton = document.querySelector(".add");
@@ -175,7 +372,7 @@ submitForm.addEventListener("submit", (event) => {
     let bookPages = document.getElementById("pages").value;
     let readStatus = document.getElementById("read").checked;
 
-    addBookToLibrary(new Book(
+    myLibrary.addBookToLibrary(new Book(
         bookTitle,
         bookAuthor,
         bookPages,
@@ -188,46 +385,28 @@ submitForm.addEventListener("submit", (event) => {
     document.getElementById("read").checked = false;
 
     const library = document.querySelector(".library");
-    let book = createBookCard(myLibrary[myLibrary.length - 1]);
+    let book = myLibrary.createBookCard(myLibrary.getBook(myLibrary.size - 1));
     library.append(book);
 
     dialog.close();
 });
 
-class Book {
-    title;
-    author;
-    pages;
-    read;
-
-    constructor (title, author, pages, read) {
-        this.title = title;
-        this.author = author;
-        this.pages = pages;
-        this.read = read;
-    }
-
-    changeReadStatus () {
-        this.read = !this.read;
-    }
-}
-
-addBookToLibrary(new Book(
+myLibrary.addBookToLibrary(new Book(
     "Night",
     "Elie Wiesel",
     "116",
     true));
 
-addBookToLibrary(new Book(
+myLibrary.addBookToLibrary(new Book(
     "1984",
     "George Orwell",
     "328",
     true));
 
-addBookToLibrary(new Book(
+myLibrary.addBookToLibrary(new Book(
     "Farenheit 451",
     "Ray Bradbury",
     "156",
     false));
 
-displayLibrary();
+    myLibrary.displayLibrary();
